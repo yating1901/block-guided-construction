285273cf6967660b9d2a9f224fa18ffa53de7b2a
zhengyating <zhengyating@mail.bnu.edu.cn>
2020-08-19 17:24:06 +0200

    delete build folder

--- src/experiment/bgc_process_data.py
+++ src/experiment/bgc_process_data.py
@@ -3,20 +3,14 @@ import pandas
 import numpy 
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
-dataset = pandas.read_hdf('bgc.hdf', 'table')
-# TODO
-# figure whether an experiment finished sucessfully automatically
-# add code to plot trajectories
-# for the successful cases, calculate standard dev, median etc of runtime
-#fix_blocks_id = ['0','1','2','3','4','5','6','7','8']
-#free_blocs_id = ['00','01','11','12','21','22','31','32']
-
+dataset1 = pandas.read_hdf('bgc.hdf', 'table')
+#dataset2 = pandas.read_hdf('dcp.hdf', 'table')
+#dataset3 = pandas.read_hdf('dcp_four_builderbots.hdf', 'table')
 
 def draw_trajectory(dataset, SEED):
     Set = dataset[dataset["SEED"] == SEED]
-    freeblock = ['11','12','13','10']
+    freeblock = ['00','01','10','11','20','21','30','31']
     for block_index in range(0, len(freeblock)):
-        #print(block_index)
         datum = []
         datum = Set[Set['ID']==freeblock[block_index]][['X','Y','Z']]
         datum = datum.values
@@ -25,7 +19,7 @@ def draw_trajectory(dataset, SEED):
         for xy_index in range(0,len(datum)):
             x_data.append(datum[xy_index][0])
             y_data.append(datum[xy_index][1])
-        plt.title('Data1 in test1 (two builderbot)')
+        plt.title('Block Guided Construction(Inert Blocks)')
         plt.plot(x_data, y_data, label='freeblock'+freeblock[block_index])
         #plt.legend()
     plt.xlabel('X axes')
@@ -33,28 +27,50 @@ def draw_trajectory(dataset, SEED):
     plt.savefig('Arena.png',bbox_inches = 'tight')
     plt.show()
 
+def Is_ended_automatically(dataset,SEED,steps):
+    #print("total steps:", steps)
+    #print("seed:",SEED)
+    Is_Ended = False
+    Set = dataset[dataset["SEED"] == SEED] 
+    freeblock = ['00','01','10','11','20','21','30','31']
+    num = 0
+    for block_index in range(0, len(freeblock)):
+        datum = []
+        datum = Set[Set['ID']==freeblock[block_index]][['X','Y','Z']]
+        datum = datum.values
+        #print(len(datum))
+        x = datum[steps-1][0]
+        y = datum[steps-1][1]
+        z = datum[steps-1][2]
+        #print(x,y,z)
+        if (round(z,3) == 0.055):
+            num = num +1
+        if num == 2:
+            Is_Ended = True
+    #print( "Is_ended_automatically:", Is_Ended)
+    return Is_Ended
 
 def box_plot(steps):
-    print(steps)
+    #print(steps)
     Data={}
-    Data['Inert Blocks'] = steps
+    Data['Inert Block'] = steps
     df = pandas.DataFrame(Data)
     def formatnum(x, pos):
         return '$%d$$k$' % (x/1000)
     formatter = FuncFormatter(formatnum)
-    df.plot.box(title="Block Guided Constuction ")
+    df.plot.box(title="Block Guided Construction")
     plt.grid(linestyle="--", alpha=0.5)
     
     #label
-    plt.ylabel('Number Of Steps')
-    plt.xlabel('Type Of Algorithms')
+    plt.ylabel('Number of Steps')
+    plt.xlabel('Number of BuilderBots')
     plt.gca().yaxis.set_major_formatter(formatter)
-    plt.savefig('bgc_inert_blocks.png',bbox_inches = 'tight')
+    plt.savefig('Bgc_One_Builderbot.png',bbox_inches = 'tight')
     plt.show()
 
 
 def calculate_length(Data):
-    freeblock_id = '10'
+    freeblock_id = '00'
     freeblock_data = Data[Data["ID"] == freeblock_id]
     step = numpy.array(freeblock_data.iloc[-1:]["STEP"])[0]   
     return step
@@ -64,12 +80,21 @@ def calculate_length(Data):
 Max_seed = 25
 data=[]
 steps = []
+freeblock = ['00','01','10','11','20','21','30','31']
 for seed in range(1,Max_seed+1):
-    data.append(dataset[dataset["SEED"] == seed])
-    steps.append(calculate_length(data[seed-1]))
+    data.append(dataset1[dataset1["SEED"] == seed])
+    steps.append(int(len(data[seed-1])/len(freeblock)))
+    #steps.append(calculate_length(data[seed-1]))
     
-steps[3] = 100
-steps[4] = 0
+    
 box_plot(steps)
-draw_trajectory(dataset,1)
+draw_trajectory(dataset1,20)
 
+
+Is_all_ended = True
+for seed in range(1, Max_seed+1):
+    print("seed:", seed,"steps:",steps[seed-1])
+   # Is_ended = Is_ended_automatically(dataset1,seed,steps[seed-1])
+ #   Is_all_ended = Is_all_ended and Is_ended
+    
+print("Is_All_Ended:", Is_all_ended)
